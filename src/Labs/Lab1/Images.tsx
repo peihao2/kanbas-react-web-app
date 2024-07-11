42b19855dd401284f2e90094bc2caedd7ef1e3b3
peihao2 <l709811890@gmail.com>
2024-07-10 17:34:01 -0700

    debug

--- src/Labs/Lab1/Images.tsx
+++ src/Labs/Lab1/Images.tsx
@@ -8,6 +8,7 @@ export default function Images() {
                 id="wd-starship"
                 width="400px"
                 src="https://www.staradvertiser.com/wp-content/uploads/2021/08/web1_Starship-gap2.jpg"
+                alt="Starship in the gap"
             />
             <br />
             Loading a local image:
@@ -16,6 +17,7 @@ export default function Images() {
                 id="wd-teslabot"
                 src="images/teslabot.jpg"
                 height="200px"
+                alt="Tesla bot"
             />
         </div>
     );
