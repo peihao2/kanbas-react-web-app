23d3e866d5d3d114bd1c339e41d55f25b7f59f3d
peihao2 <l709811890@gmail.com>
2024-07-28 23:56:38 -0700

    a4

--- src/Labs/Lab3/index.tsx
+++ src/Labs/Lab3/index.tsx
@@ -31,56 +31,67 @@ import Square from "./Square";
 import Highlight from "./Highlight";
 import AddPathParameters from "./AddPathParameters";
 import PathParameters from "./PathParameters";
-
+import { useSelector } from "react-redux";
 
 export default function Lab3() {
-    console.log('Hello World!');
+  const { todos } = useSelector((state: any) => state.todosReducer);
 
-    return (
-        <div id="wd-lab3" className="container-fluid">
-            <h3>Lab 3</h3>
-            <VariablesAndConstants />
-            <VariableTypes />
-            <BooleanVariables />
-            <IfElse />
-            <TernaryOperator />
-            <ConditionalOutputIfElse />
-            <ConditionalOutputInline />
-            <LegacyFunctions />
-            <ArrowFunctions />
-            <ImpliedReturn />
-            <TemplateLiterals />
-            <SimpleArrays />
-            <ArrayIndexAndLength />
-            <AddingAndRemovingToFromArrays />
-            <ForLoops />
-            <MapFunction />
-            <FindFunction />
-            <FilterFunction />
-            <JsonStringify />
-            <House />
-            <TodoItem />
-            <TodoList />
-            <Spreading />
-            <Destructing />
-            <FunctionDestructing />
-            <DestructingImports />
-            <Classes />
-            <Styles />
-            <Add a={3} b={4} />
+  console.log("Hello World!");
 
-            <h4>Square of 4</h4>
-            <Square>4</Square>
-            <hr />
+  return (
+    <div id="wd-lab3" className="container-fluid">
+      <h3>Lab 3</h3>
+      <ul className="list-group">
+        {todos.map((todo: any) => (
+          <li className="list-group-item" key={todo.id}>
+            {todo.title}
+          </li>
+        ))}
+      </ul>
+      <hr />
+      <VariablesAndConstants />
+      <VariableTypes />
+      <BooleanVariables />
+      <IfElse />
+      <TernaryOperator />
+      <ConditionalOutputIfElse />
+      <ConditionalOutputInline />
+      <LegacyFunctions />
+      <ArrowFunctions />
+      <ImpliedReturn />
+      <TemplateLiterals />
+      <SimpleArrays />
+      <ArrayIndexAndLength />
+      <AddingAndRemovingToFromArrays />
+      <ForLoops />
+      <MapFunction />
+      <FindFunction />
+      <FilterFunction />
+      <JsonStringify />
+      <House />
+      <TodoItem />
+      <TodoList />
+      <Spreading />
+      <Destructing />
+      <FunctionDestructing />
+      <DestructingImports />
+      <Classes />
+      <Styles />
+      <Add a={3} b={4} />
 
-            <Highlight>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum, saepe totam
-                vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident voluptates.
-            </Highlight>     
+      <h4>Square of 4</h4>
+      <Square>4</Square>
+      <hr />
 
-            <AddPathParameters/> 
-            <PathParameters/>  
+      <Highlight>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione
+        eaque illo minus cum, saepe totam vel nihil repellat nemo explicabo
+        excepturi consectetur. Modi omnis minus sequi maiores, provident
+        voluptates.
+      </Highlight>
 
-            </div>
-    );
+      <AddPathParameters />
+      <PathParameters />
+    </div>
+  );
 }
