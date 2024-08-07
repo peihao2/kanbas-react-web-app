5311981f9a9341e766676940d6025860fd3661ed
peihao2 <l709811890@gmail.com>
2024-08-06 17:28:19 -0700

    a5

--- src/Labs/Lab5/index.tsx
+++ src/Labs/Lab5/index.tsx
@@ -1,3 +1,13 @@
+import EncodingParametersInURLs from "./EncodingParametersInURLs";
+import EnvironmentVariables from "./EnvironmentVariables";
+import HttpClient from "./HttpClient";
+import PathParameters from "./PathParameters";
+import QueryParameters from "./QueryParameters";
+import WorkingWithArrays from "./WorkingWithArrays";
+import WorkingWithArraysAsynchronously from "./WorkingWithArraysAsynchronously";
+import WorkingWithObjects from "./WorkingWithObjects";
+import WorkingWithObjectsAsynchronously from "./WorkingWithObjectsAsynchronously";
+
 export default function Lab5() {
   return (
     <div id="wd-lab5">
@@ -9,6 +19,17 @@ export default function Lab5() {
         >
           Welcome
         </a>
+
+        <h2>Calculator</h2>
+        <EncodingParametersInURLs />
+        <EnvironmentVariables />
+        <PathParameters />
+        <QueryParameters />
+        <WorkingWithObjects />
+        <WorkingWithArrays />
+        <HttpClient />
+        <WorkingWithObjectsAsynchronously />
+        <WorkingWithArraysAsynchronously />
       </div>
       <hr />
     </div>
