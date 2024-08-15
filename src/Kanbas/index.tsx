f4bbec22c53f95e726d76f6b2e31b94f7e15ae42
peihao2 <l709811890@gmail.com>
2024-08-15 00:09:49 -0700

    a6

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -8,6 +8,7 @@ import { useState, useEffect } from "react";
 import store from "./store";
 import { Provider } from "react-redux";
 import * as client from "./Courses/client";
+import Account from "./Account";
 
 export default function Kanbas() {
   const [courses, setCourses] = useState<any[]>([]);
@@ -59,8 +60,10 @@ export default function Kanbas() {
           </div>
           <div className="flex-fill p-4">
             <Routes>
+              <Route path="/Account/*" element={<Account />} />
+
               <Route path="/" element={<Navigate to="Dashboard" />} />
-              <Route path="Account" element={<h1>Account</h1>} />
+              <Route path="Account/*" element={<Account />} />
               <Route
                 path="Dashboard"
                 element={
--- src/Kanbas/store.ts
+++ src/Kanbas/store.ts
@@ -1,13 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
 import modulesReducer from "./Courses/Modules/reducer";
 import assignmentsReducer from "./Courses/Assignments/reducer";
+import accountReducer from "./Account/reducer";
 
 const store = configureStore({
   reducer: {
     modulesReducer,
     assignments: assignmentsReducer,
+    accountReducer,
   },
 });
 export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
 
 export default store;
